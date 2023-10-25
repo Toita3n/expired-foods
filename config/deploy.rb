2592d766503e1c1737a1e5d99194f21776b759ac
Toita3n <toita0920@gmail.com>
2023-10-25 13:28:17 +0000

    modified_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :deploy_to, '/var/www/expired-foods'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Toita3n/expired-foods.git'
-keys: ['~/.ssh/runteq-key.pem']
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
