47d7cf24d5ac6ca81c25d92a7fd86452daf2f41a
Toita3n <toita0920@gmail.com>
2023-10-31 06:22:33 +0000

    change_unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '54.64.14.201', user: 'ec2-user', roles: %w{app db web}
+server '172.31.0.188', user: 'ec2-user', roles: %w{app db web}
 
 
 # role-based syntax
