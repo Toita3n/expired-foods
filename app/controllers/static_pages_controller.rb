1d0739429dff913b46964d34007b0f84e717d2d0
Toita3n <toita0920@gmail.com>
2023-11-15 17:44:36 +0900

    indexファイル削除

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,6 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login
 
-  def index
-    render 'top'
-  end
-
   def top;end
 
   def term_of_use;end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#top'
-  get '/index.html', to: 'static_pages#index'
   get 'sessions/new'
   get '/privacy_policy', to: 'static_pages#privacy_policy'
   get '/term_of_use', to: 'static_pages#term_of_use'
