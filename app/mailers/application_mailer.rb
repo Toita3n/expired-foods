facca6c0c39338c47a76da86ecff6db2d9f1537d
Toita3n <toita0920@gmail.com>
2023-11-21 04:38:49 +0000

    mailer変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'notifications@expired-foods.com'
+  default from: 'expired.foodsapp@gmail.com'
   layout 'mailer'
 end
--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -3,6 +3,6 @@ class InquiryMailer < ApplicationMailer
   def inquiry_mail(inquiry)
     @inquiry = inquiry
     @greeting = 'お問い合わせありがとうございます'
-    mail(to: 'notifications@expired-foods.com', subject: 'お問い合わせ')
+    mail(to: 'expired.foodsapp@gmail.com', subject: 'お問い合わせ')
   end
 end
